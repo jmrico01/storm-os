8828edd3b91b8c569b40f53b7ca97ae18867476c
Jose M Rico <jrico@princeton.edu>
2017-12-18 00:27:05 -0500

    Added & loaded C kernel

--- compile/compile.sh
+++ compile/compile.sh
@@ -4,4 +4,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC_DIR="$SCRIPT_DIR/../src"
 BUILD_DIR="$SCRIPT_DIR/../build"
 
-nasm "$SRC_DIR/boot0.s" -f bin -o "$BUILD_DIR/boot0"
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR > /dev/null
+
+# Assemble boot sector code
+nasm "$SRC_DIR/boot.s" -f bin -o boot.bin
+
+# Assemble kernel entry
+nasm "$SRC_DIR/kernel_entry.s" -f elf -o kernel_entry.o
+
+# Compile and link main kernel code
+gcc -ffreestanding -m32 -c "$SRC_DIR/kernel.c" -o kernel.o
+ld -o kernel.bin -m elf_i386 -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+
+# Create disk image
+cat boot.bin kernel.bin > os_image
+
+popd > /dev/null
---
+++ src/kernel.c
@@ -0,0 +1,7 @@
+void main()
+{
+    char* videoMemory = (char*)0xb8000;
+    *videoMemory = 'X';
+
+    while(1);
+}
